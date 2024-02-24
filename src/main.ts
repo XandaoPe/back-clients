7320b05bb4645564065a231df06f6bffc2ed7b63
XandaoPe <73856144+XandaoPe@users.noreply.github.com>
2024-02-24 00:17:22 -0300

    cats app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
+app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
